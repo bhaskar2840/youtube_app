208cad9646c80ca979cb342d09a50fc0c4ce1887
Bhaskar Jha <47430433+bhaskar2840@users.noreply.github.com>
2024-01-22 20:33:48 +0530

    debugging

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import express from "express";
-// import cookieParser from "cookie-parser";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -10,4 +9,15 @@ app.use(express.json({ limit: "16kb" })); // used for form data
 app.use(express.urlencoded({ limit: "16kb" })); // used for url reading and data from url {extended:true} used to nest object.
 app.use(express.static("public")); // use to access static files in public folder
 app.use(cookieParser());
-export { app };
+
+//todo importing router
+
+import userRouter from "./routers/user.routes.js";
+
+//routes declaration
+//! we will not use app.get() as we are passing route as middleware and not directly.
+
+app.use("api/v1/users", userRouter);
+// http://localhost:8000/api/v1/users/register         api/v2/users is standard practise.
+
+export default app;
